c03eab6fe5341ee547e75966d11e9b9826cf6f9f
twinther <tommy@winther.nu>
2012-09-09 09:58:36 +0200

    Version 1.0.2 - Updated to match site changes - Changed video streams to use M3U8 links - Now using script.module.buggalo

--- addon.py
+++ addon.py
@@ -1,98 +1,119 @@
+#
+#      Copyright (C) 2012 Tommy Winther
+#      http://tommy.winther.nu
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this Program; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+#
 import os
 import sys
 import urlparse
 import urllib2
 import re
 import datetime
-from elementtree import ElementTree
+
+import buggalo
 
 import xbmcgui
 import xbmcaddon
 import xbmcplugin
 
-CATEGORIES = {
-    30100 : '',
-    30101 : 'investor.html',
-    30102 : 'privatoekonomi.html',
-    30103 : 'oekonomi.html',
-    30104 : 'politik.html',
-    30105 : 'it.html',
-    30106 : 'karriere.html',
-    30107 : 'weekend.html'
-}
-
-URL = 'http://borsentv.dk/%s'
-VIDEO_URL = 'http://borsentv.dk/services/frontend/borsentv/mFrontendT2_latestClips.php?%s'
-RTMP_URL = 'rtmp://stream.borsentv.dk/arkiv'
-RTMP_PATH = 'borsen_tv/mp4:%s.mp4'
+BASE_URL = 'http://borsen.dk/tv/'
+MONTHS = ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov','dec']
 
 class BorsenTVAddon(object):
     def showCategories(self):
-        for id, slug in CATEGORIES.items():
-            item = xbmcgui.ListItem(ADDON.getLocalizedString(id), iconImage = ICON)
-            xbmcplugin.addDirectoryItem(HANDLE, PATH + '?category=%d' % id, item, isFolder = True)
-
-        xbmcplugin.endOfDirectory(HANDLE)
-
-    def showCategory(self, id):
-        url = URL % CATEGORIES[int(id)]
-        u = urllib2.urlopen(url)
+        u = urllib2.urlopen(BASE_URL)
         html = u.read()
         u.close()
 
-        xml = ''
-        m = re.search("'(perpage=[^']+)'", html)
-        if m:
-            url = VIDEO_URL % m.group(1).replace('%26', '&')
-            u = urllib2.urlopen(url)
-            xml = u.read()
-            u.close()
+        for m in re.finditer('href="/tv/([^"]+)".*?>([^<]+)<', html):
+            id = m.group(1)
+            title = m.group(2)
+            item = xbmcgui.ListItem(title, iconImage = ICON)
+            xbmcplugin.addDirectoryItem(HANDLE, PATH + '?category=%s' % id, item, isFolder = True)
 
+        xbmcplugin.endOfDirectory(HANDLE)
 
-        dom = ElementTree.fromstring(xml)
-        for video in dom.find('videos/tab'):
-            sThumb = video.findtext('sthumb').replace('_125.jpg', '.jpg')
-            date = self._parseDate(video.findtext('hour'))
+    def showCategory(self, path):
+        u = urllib2.urlopen(BASE_URL + path)
+        html = u.read()
+        u.close()
 
-            item = xbmcgui.ListItem(video.findtext('title'), iconImage = sThumb, thumbnailImage = sThumb)
-            item.setProperty('Fanart_Image', sThumb)
+        for m in re.finditer('class="time">(.*?)</span>.*?href="([^"]+)" title="([^"]+)"', html, re.DOTALL):
+            dateStr = m.group(1).strip()
+            url = m.group(2)
+            title = m.group(3)
+
+            date = self._parseDate(dateStr)
+
+            item = xbmcgui.ListItem(title, iconImage = ICON)
             item.setInfo('video', infoLabels = {
-                'title' : video.findtext('title'),
-                'plot' : video.findtext('description').replace('<br />', ''),
+                'title' : title,
                 'date' : date.strftime('%d.%m.%Y'),
                 'aired' : date.strftime('%d-%m-%Y'),
                 'studio' : ADDON.getAddonInfo('name'),
                 'year' : int(date.strftime('%Y'))
             })
 
-            # Parse video id from file_url
-            m = re.search('([0-9]+)$', video.findtext('file_url'))
-            url = RTMP_PATH % m.group(1)
-            xbmcplugin.addDirectoryItem(HANDLE, RTMP_URL + ' playpath=' + url, item)
+            item.setProperty('IsPlayable', 'true')
+            xbmcplugin.addDirectoryItem(HANDLE, PATH + '?video=%s' % url, item, isFolder = False)
 
         xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
         xbmcplugin.endOfDirectory(HANDLE)
 
+    def playVideo(self, url):
+        u = urllib2.urlopen(url)
+        html = u.read()
+        u.close()
+
+        m = re.search('poster="([^"]+)".*?source src="([^"]+)" type="video/mp4"', html)
+        poster = m.group(1)
+        videoUrl = m.group(2)
+
+        item = xbmcgui.ListItem(path=videoUrl, thumbnailImage=poster)
+        xbmcplugin.setResolvedUrl(HANDLE, True, item)
+
     def _parseDate(self, input):
         d = datetime.datetime.now()
-        if input.find('/') > 0:
-            m = re.search('([0-9]+)/([0-9]+)', input)
-            d = datetime.datetime(year = d.year, month = int(m.group(2)), day = int(m.group(1)))
+        m = re.search('(\d{1,2})\. ([a-z]{3})\.', input)
+        if m is not None:
+            month = MONTHS.index(m.group(2)) + 1
+            year = d.year
+            if month > d.month:
+                year -= 1
+            d = datetime.datetime(year = year, month = month, day = int(m.group(1)))
 
         return d
 
 if __name__ == '__main__':
-    ADDON = xbmcaddon.Addon(id = 'plugin.video.borsentv.dk')
+    ADDON = xbmcaddon.Addon()
     PATH = sys.argv[0]
     HANDLE = int(sys.argv[1])
     PARAMS = urlparse.parse_qs(sys.argv[2][1:])
 
     ICON = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
 
-    btv = BorsenTVAddon()
-    if PARAMS.has_key('category'):
-        btv.showCategory(PARAMS['category'][0])
-    else:
-        btv.showCategories()
-
-
+    buggalo.SUBMIT_URL = 'http://tommy.winther.nu/exception/submit.php'
+    try:
+        btv = BorsenTVAddon()
+        if PARAMS.has_key('category'):
+            btv.showCategory(PARAMS['category'][0])
+        elif PARAMS.has_key('video'):
+            btv.playVideo(PARAMS['video'][0])
+        else:
+            btv.showCategories()
+    except Exception:
+        buggalo.onExceptionRaised()
