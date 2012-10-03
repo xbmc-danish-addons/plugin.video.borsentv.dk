0822c104e46408034e0eaa6b27a2954ecf115503
twinther <tommy@winther.nu>
2012-10-03 19:39:14 +0200

    Changed addon to use RSS feeds to greatly simplify the codebase

--- addon.py
+++ addon.py
@@ -31,89 +31,32 @@ import xbmcaddon
 import xbmcplugin
 
 BASE_URL = 'http://borsen.dk/tv/'
-MONTHS = ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov','dec']
+FEEDS = {
+    30100 : 'rss://borsen.dk/podcast/alle',
+    30108 : 'rss://borsen.dk/podcast/investor1000',
+    30101 : 'rss://borsen.dk/podcast/investor',
+    30106 : 'rss://borsen.dk/podcast/karriere',
+    30104 : 'rss://borsen.dk/podcast/politik',
+    30103 : 'rss://borsen.dk/podcast/politik',
+    30102 : 'rss://borsen.dk/podcast/politik'
+}
 
 class BorsenTVAddon(object):
     def showCategories(self):
-        u = urllib2.urlopen(BASE_URL)
-        html = u.read()
-        u.close()
-
-        for m in re.finditer('href="/tv/([^"]+)".*?>([^<]+)<', html):
-            id = m.group(1)
-            title = m.group(2)
-            item = xbmcgui.ListItem(title, iconImage = ICON)
-            xbmcplugin.addDirectoryItem(HANDLE, PATH + '?category=%s' % id, item, isFolder = True)
+        for id in FEEDS.keys():
+            item = xbmcgui.ListItem(ADDON.getLocalizedString(id), iconImage = ICON)
+            xbmcplugin.addDirectoryItem(HANDLE, FEEDS[id], item, isFolder = True)
 
         xbmcplugin.endOfDirectory(HANDLE)
-
-    def showCategory(self, path):
-        u = urllib2.urlopen(BASE_URL + path)
-        html = u.read()
-        u.close()
-
-        for m in re.finditer('class="time">(.*?)</span>.*?href="([^"]+)" title="([^"]+)"', html, re.DOTALL):
-            dateStr = m.group(1).strip()
-            url = m.group(2)
-            title = m.group(3)
-
-            date = self._parseDate(dateStr)
-
-            item = xbmcgui.ListItem(title, iconImage = ICON)
-            item.setInfo('video', infoLabels = {
-                'title' : title,
-                'date' : date.strftime('%d.%m.%Y'),
-                'aired' : date.strftime('%d-%m-%Y'),
-                'studio' : ADDON.getAddonInfo('name'),
-                'year' : int(date.strftime('%Y'))
-            })
-
-            item.setProperty('IsPlayable', 'true')
-            xbmcplugin.addDirectoryItem(HANDLE, PATH + '?video=%s' % url, item, isFolder = False)
-
-        xbmcplugin.addSortMethod(HANDLE, xbmcplugin.SORT_METHOD_DATE)
-        xbmcplugin.endOfDirectory(HANDLE)
-
-    def playVideo(self, url):
-        u = urllib2.urlopen(url)
-        html = u.read()
-        u.close()
-
-        m = re.search('poster="([^"]+)".*?source src="([^"]+)" type="video/mp4"', html)
-        poster = m.group(1)
-        videoUrl = m.group(2)
-
-        item = xbmcgui.ListItem(path=videoUrl, thumbnailImage=poster)
-        xbmcplugin.setResolvedUrl(HANDLE, True, item)
-
-    def _parseDate(self, input):
-        d = datetime.datetime.now()
-        m = re.search('(\d{1,2})\. ([a-z]{3})\.', input)
-        if m is not None:
-            month = MONTHS.index(m.group(2)) + 1
-            year = d.year
-            if month > d.month:
-                year -= 1
-            d = datetime.datetime(year = year, month = month, day = int(m.group(1)))
-
-        return d
 
 if __name__ == '__main__':
     ADDON = xbmcaddon.Addon()
-    PATH = sys.argv[0]
     HANDLE = int(sys.argv[1])
-    PARAMS = urlparse.parse_qs(sys.argv[2][1:])
-
     ICON = os.path.join(ADDON.getAddonInfo('path'), 'icon.png')
 
     buggalo.SUBMIT_URL = 'http://tommy.winther.nu/exception/submit.php'
     try:
         btv = BorsenTVAddon()
-        if PARAMS.has_key('category'):
-            btv.showCategory(PARAMS['category'][0])
-        elif PARAMS.has_key('video'):
-            btv.playVideo(PARAMS['video'][0])
-        else:
-            btv.showCategories()
+        btv.showCategories()
     except Exception:
         buggalo.onExceptionRaised()
